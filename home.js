7c862bb65fa6c9d14d3f468915b5c10df2bb2dc3
ekremucuncu <ekremkemalucuncu@gmail.com>
2022-02-09 07:02:05 +0300

    Last

--- home.js
+++ home.js
@@ -1,14 +1,40 @@
 
+// scroll from landing-section to we-section
+$("#scroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#we-section").offset().top
+    }, 0);
+});
 
 
+// js effects for two-page
 $("#cozumler").click(function () {
   $("#first").css("display", "");
   $("#second").css("display", "none");
   $("#bgi").attr("class", "background-image-1");
+  $("#cozumler").attr("class", "float-end cursor underline-responsive");
+  $("#hizmetler").attr("class", "float-start cursor");
 });
 
+// js effects for two-page
 $("#hizmetler").click(function () {
   $("#first").css("display", "none");
   $("#second").css("display", "");
   $("#bgi").attr("class", "background-image-2");
+  $("#hizmetler").attr("class", "float-start cursor underline-responsive");
+  $("#cozumler").attr("class", "float-end cursor");
+});
+
+$( document ).ready(function() {
+    var height =$('#long-text').css('height');
+    $('.cart-text').css("height",height);
+});
+
+$(window).on("scroll", function() {
+    var scrollPos = $(window).scrollTop();
+    if (scrollPos <= 0) {
+        $("#go-top").fadeOut();
+    } else {
+        $("#go-top").fadeIn();
+    }
 });
